241c8da6d1fe45f3d7b5ded5c2eaf0e53c3acddc
Harshith <menteharshith@gmail.com>
2023-11-02 16:41:38 +0530

    Update Image Tag

--- components/home/projects.tsx
+++ components/home/projects.tsx
@@ -1,13 +1,13 @@
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { useContext } from 'react';
-import TextCards from '../../shared/components/text-cards';
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useContext } from "react";
+import TextCards from "../../shared/components/text-cards";
 import HeaderSmall from "../../shared/components/header-small";
 import Button from "../../shared/components/buttons";
 import ProjectCard from "../../shared/components/project-card";
-import project from '@utils/projects';
+import project from "@utils/projects";
+import Image from "next/image";
 type Props = {};
-
 
 export default function Projects({}: Props): JSX.Element {
   const router = useRouter();
@@ -15,19 +15,21 @@ export default function Projects({}: Props): JSX.Element {
     <>
       <div>
         <div className="relative h-auto py-10 lg:py-20">
-          <img
+          <Image
             src="/images/vectors/heart.svg"
             alt="Circle Vector"
             className="absolute -right-50p md:-right-96 bottom-16 md:-bottom-20 pointer-events-none animate-pulse"
+            layout="fill"
           />
           <div className="ml-8 sm:mx-12 md:mx-16 grid grid-cols-12 gap-4 h-auto place-items-center items-center">
             <div className="col-span-full 3xl:-ml-24 md:col-span-7 lg:col-span-6 flex flex-col justify-center items-center">
               {/* Hero Header */}
               <div className="items-center px-12  relative">
-                <img
+                <Image
                   src="/images/vectors/boxes.svg"
                   alt="Box Vector"
                   className="block absolute right-0 top-0 w-20 md:w-16 lg:w-52 opacity-60"
+                  layout="fill"
                 />
                 <div className="md:-ml-0 -ml-24">
                   <HeaderSmall text="Let’s walk the talk!" />
@@ -37,18 +39,20 @@ export default function Projects({}: Props): JSX.Element {
                 <h1 className="hidden md:block mb-3 md:mt-0 text-4xl sm:text-2xl md:text-3xl lg:text-2xl xl:text-1xl font-extrabold text-white leading-none ">
                   Projects I <span className="text-pink font-bold">built.</span>
                 </h1>
-                <div className="sm:ml-52 xl:ml-96 transform rotate-90">
+                <div className="sm:ml-52 xl:ml-96 transform rotate-90 ">
                   <Link href="#projects">
-                    <img
+                    <Image
                       src="/images/vectors/arrows-right.svg"
                       alt="Next"
-                      className="hidden lg:block w-14 lg:mt-24 xl:mt-16 sm:mt-0 transform-all animate-translateright "
+                      className="hidden lg:block w-14 lg:mt-28 xl:mt-20 sm:mt-8 transform-all animate-translateright"
+                      layout="fixed"
+                      width={48}
+                      height={48}
                     />
                   </Link>
                 </div>
               </div>
             </div>
-            {/* FIXME Make thie cards of this page dyanmic */}
             <div className="relative  col-span-12  md:col-span-5 lg:col-span-6 flex flex-col ">
               <div className="w-64  text-white h-auto shadow-light-xl hover:shadow-light-3xl transform transition-all duration-300 hover:scale-110">
                 <TextCards
--- components/home/skills-all.tsx
+++ components/home/skills-all.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Skillsall from "../../shared/utils/skillsall";
 import { Skill } from "../../shared/utils/types";
 
@@ -6,11 +7,14 @@ export default function SkillsIcons(): JSX.Element {
     <div className="flex items-center justify-center lg:mt-16 flex-wrap w-5/6 md:w-full xl:w-5/6">
       {Skillsall.map(({ src, name }: Skill) => {
         return (
-          <img
+          <Image
             src={src}
             alt={name}
             key={src}
-            className="p-2 tranform transition duration-500 hover:scale-125 sm:p-6 h-16 sm:h-24"
+            className="p-2 transform transition duration-500 hover:scale-125 sm:p-6 h-16 sm:h-24"
+            layout="fixed"
+            width={100}
+            height={100}
           />
         );
       })}
