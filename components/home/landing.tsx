c9bc3804210527322794001d55307570d183307c
Harshith Mente <109957201+joeyyy09@users.noreply.github.com>
2024-09-05 15:35:21 +0530

    Update landing.tsx

--- components/home/landing.tsx
+++ components/home/landing.tsx
@@ -59,7 +59,7 @@ export default function Landing(): JSX.Element {
                     text="Know More"
                     onClickHandler={() => router.push("#skills")}
                   />
-                  <a href="https://drive.google.com/file/d/1Tmp45BF9EjJ78jh4jOWUWhcgaeUYvoPl/view?usp=sharing">
+                  <a href="https://drive.google.com/file/d/1DxSG2w89FcUbfz1CowLIVMFBlhZPpZA3/view?usp=sharing">
                     <Button
                       type="outlined"
                       text="Download Resume"
